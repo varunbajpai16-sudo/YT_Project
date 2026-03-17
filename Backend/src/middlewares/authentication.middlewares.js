345586d44ea7152bb44720f271cb63c90dfad72b
Varun Bajpai <your_email@gmail.com>
2026-03-17 21:04:07 +0530

    Backend Changes

--- Backend/src/middlewares/authentication.middlewares.js
+++ Backend/src/middlewares/authentication.middlewares.js
@@ -1,34 +1,35 @@
 import apierror from "../utils/apierror.utils.js";
 import jwt from "jsonwebtoken";
-import asynchandler from '../utils/asynchandler.uitls.js'
-import { User } from '../models/user.models.js'
+import asynchandler from "../utils/asynchandler.uitls.js";
+import { User } from "../models/user.models.js";
 
-export const verifytoken = asynchandler(async(req,__,next)=>{
+export const verifytoken = asynchandler(async (req, __, next) => {
+  const token =
+    req.cookies?.accesstoken ||
+    (req.header("Authorization")?.startsWith("Bearer ")
+      ? req.header("Authorization").replace("Bearer ", "")
+      : null);
 
-    const token = req.cookies?.accesstoken || req.header("Authorization")?.replace("Bearer ", "")
+  if (!token) {
+    throw new apierror(401, "Unauthorized, token not found");
+  }
 
-    if(!token){
-        console.log("no token")
-        throw new apierror(401,"Unauthorized, token not found")
-    }
+  let decodedtoken;
 
-    
-    const decodedtoken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+  try {
+    decodedtoken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  } catch (error) {
+    throw new apierror(401, "Invalid or expired token");
+  }
 
-    if(!decodedtoken){
-        throw new apierror(401,"Unauthorized, invalid token")
-    }
+  const user = await User.findById(decodedtoken._id).select(
+    "-password -refreshTokens"
+  );
 
-    const user = await User.findById(decodedtoken._id).select("-password -refreshTokens")
+  if (!user) {
+    throw new apierror(404, "User not found");
+  }
 
-    if(user.accesstoken!==0){
-        throw new apierror(401,"Unauthorized, token expired")
-    }
-    
-    if(!user){
-        throw new apierror(404,"User not found")
-    }
-
-    req.user = user;
-    next()
-})
+  req.user = user;
+  next();
+});
