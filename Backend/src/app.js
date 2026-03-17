90c6f59e4aa2320163c38a5747a38952ee85a4d6
Varun Bajpai <your_email@gmail.com>
2026-03-17 22:37:55 +0530

    Routing change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,43 +1,50 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import userRouter from "./Routes/user.routes.js";
-import videoRouter from "./Routes/video.routes.js";
-import subscriptionRouter from "./Routes/subscription.routes.js";
-import playlistrouter from "../src/Routes/playlist.routes.js"
-import commentsrouter from "../src/Routes/comments.routes.js"
-import likesrouter from "../src/Routes/like.routes.js"
-import { swaggerUi, specs } from "./swagger.js";
-const app = express();
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import userRouter from './Routes/user.routes.js'
+import videoRouter from './Routes/video.routes.js'
+import subscriptionRouter from './Routes/subscription.routes.js'
+import playlistrouter from '../src/Routes/playlist.routes.js'
+import commentsrouter from '../src/Routes/comments.routes.js'
+import likesrouter from '../src/Routes/like.routes.js'
+import { swaggerUi, specs } from './swagger.js'
+import path from 'path'
+const app = express()
 
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-  }) 
-);
+  }),
+)
 
-app.use(express.json({ limit: "50mb" }));
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.static("public"));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use(express.json({ limit: '50mb' }))
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+app.use(express.static('public'))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
-app.get("/", (req, res) => {
-  res.send("API is working");
-});
+app.get('/', (req, res) => {
+  res.send('API is working')
+})
 
-app.use("/api/v1/user", userRouter);
+app.use('/api/v1/user', userRouter)
 
-app.use("/api/v1/video", videoRouter);
+app.use('/api/v1/video', videoRouter)
 
-app.use("/api/v1/subscription", subscriptionRouter);
+app.use('/api/v1/subscription', subscriptionRouter)
 
-app.use("/api/v1/playlist",playlistrouter)
+app.use('/api/v1/playlist', playlistrouter)
 
-app.use("/api/v1/comments",commentsrouter)
+app.use('/api/v1/comments', commentsrouter)
 
-app.use("/api/v1/likes",likesrouter)
+app.use('/api/v1/likes', likesrouter)
 
+const __dirname = path.resolve()
 
-export default app;
+app.use(express.static(path.join(__dirname, '../Frontend/dist')))
+
+app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, '../Frontend', 'dist', 'index.html'))
+}) 
+export default app
